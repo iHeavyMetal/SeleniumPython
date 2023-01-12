341336be7c39bdc1c97d40ac51f2351fd82068b1
HeavyMetal <dziobal294@gmail.com>
2023-01-12 18:39:01 +0100

    Update script. Add Note

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -1,5 +1,20 @@
+import time
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("http://www.google.pl")
+#driver.maximize_window() #set_window_size(1920,1080)
+
+time.sleep(5)
+
+#driver.find_element(By.CLASS_NAME, "RNNXgb")
+driver.find_element(By.NAME, "q").send_keys("selenium" + Keys.ENTER)
+
+
+time.sleep(3)
+
---
+++ note.py
@@ -0,0 +1,11 @@
+
+
+
+ID - (By.ID, "example")
+CSS_SELECTOR - (By.CSS_SELECTOR, '[name="q"]')
+NAME - (By.NAME, 'q')
+LINK_TEXT - (By.LINK_TEXT, "example") #in <a href> tag
+PARTIAL_LINK_TEXT - (By.PARTIAL_LINK_TEXT, "example")
+CLASS_NAME - (By.CLASS_NAME, "example") #class="example"
+
+
