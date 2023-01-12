0e225f1db55ef773b716b10102e0f901c98bac70
HeavyMetal <dziobal294@gmail.com>
2023-01-12 19:05:05 +0100

    Update script. Add Note + CSS_SELECTORS.doc

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -11,9 +11,13 @@ driver.get("http://www.google.pl")
 #driver.maximize_window() #set_window_size(1920,1080)
 
 time.sleep(5)
-
+#driver.find_element(By.TAG_NAME, "img")
 #driver.find_element(By.CLASS_NAME, "RNNXgb")
+driver.find_element(By.CSS_SELECTOR, "a")
+driver.find_element(By.CSS_SELECTOR, "img")
+
 driver.find_element(By.NAME, "q").send_keys("selenium" + Keys.ENTER)
+
 
 
 time.sleep(3)
--- note.py
+++ note.py
@@ -3,9 +3,22 @@
 
 ID - (By.ID, "example")
 CSS_SELECTOR - (By.CSS_SELECTOR, '[name="q"]')
+CSS_SELECTOR - driver.find_element((By.CSS_SELECTOR, "th:first-child").text) #table(th). print table content
 NAME - (By.NAME, 'q')
 LINK_TEXT - (By.LINK_TEXT, "example") #in <a href> tag
 PARTIAL_LINK_TEXT - (By.PARTIAL_LINK_TEXT, "example")
-CLASS_NAME - (By.CLASS_NAME, "example") #class="example"
+CLASS_NAME - (By.CLASS_NAME, "example") #class="example1 example2" - can search by example1 or example2
+TAG_NAME - (By.TAG_NAME, "example") # "a", "p", "img", "div", "label"
 
-
+CSS_SELECTOR:
+    a
+    .red
+     #fname
+     div.row
+    [aria-hidden="true"]
+    li a
+    li+a
+    li,a
+    p:first-child
+    p:last-child
+    p:nth-child(2)
