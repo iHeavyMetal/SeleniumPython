3add2e20b8590b6e978e352ee30c2bb6eb8943b3
HeavyMetal <dziobal294@gmail.com>
2023-01-13 11:28:03 +0100

    Update script.

--- alert_local_page.py
+++ alert_local_page.py
@@ -1,7 +1,9 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
@@ -10,12 +12,21 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("file://C:/Users/godte/Desktop/Projects/SeleniumPython/files/Test.html")
 
 driver.find_element(By.ID, "clickOnMe").click()
-time.sleep(1)
+#time.sleep(1)
 driver.switch_to.alert.accept()
 time.sleep(3)
 driver.find_element(By.ID, "clickOnMe").click()
-time.sleep(1)
+#time.sleep(1)
 driver.switch_to.alert.dismiss()
 
+#driver.find_element(By.NAME, "password").send_keys(Keys.ENTER) # sent key ENTER to password field
+
+car_select = Select(driver.find_element(By.TAG_NAME, "select"))
+#car_select.select_by_visible_text("Volvo") #by text
+car_select.select_by_value("saab")          #by value
+#car_select.select_by_index(0)              #by index
+
+driver.find_element(By.XPATH, "//input[@type='checkbox']").click() #checkbox
+driver.find_element(By.XPATH, "//input[@value='male']").click()   #radio. can choose multiple options
 
 time.sleep(3)
