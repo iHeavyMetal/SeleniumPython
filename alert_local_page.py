2a43179c66daee889dafedfa7fb0c2c672fb09c2
HeavyMetal <dziobal294@gmail.com>
2023-01-13 11:53:43 +0100

    import time
    from selenium import webdriver
    from selenium.webdriver import Keys
    Switch between windows

--- alert_local_page.py
+++ alert_local_page.py
@@ -14,10 +14,13 @@ driver.get("file://C:/Users/godte/Desktop/Projects/SeleniumPython/files/Test.htm
 driver.find_element(By.ID, "clickOnMe").click()
 #time.sleep(1)
 driver.switch_to.alert.accept()
-time.sleep(3)
+#time.sleep(3)
 driver.find_element(By.ID, "clickOnMe").click()
 #time.sleep(1)
 driver.switch_to.alert.dismiss()
+
+driver.find_element(By.ID,"fname").send_keys("Selenium")
+print("fname text is " + driver.find_element(By.ID, "fname").get_attribute("value")) #get text from input
 
 #driver.find_element(By.NAME, "password").send_keys(Keys.ENTER) # sent key ENTER to password field
 
@@ -28,5 +31,14 @@ car_select.select_by_value("saab")          #by value
 
 driver.find_element(By.XPATH, "//input[@type='checkbox']").click() #checkbox
 driver.find_element(By.XPATH, "//input[@value='male']").click()   #radio. can choose multiple options
+print(driver.find_element(By.TAG_NAME, "h1").text) # print text from specific tag
+print(driver.find_element(By.ID, "clickOnMe").text) # print text from specific tag
+print(driver.find_element(By.TAG_NAME, "p").text)   #can`t get text from hidden element!!
+print(driver.find_element(By.TAG_NAME, "p").get_attribute("textContent")) #<<< get text from hidden element
+
+print(driver.find_element(By.ID, "smileImage").size.get("height")) #Size of current item
+print(driver.find_element(By.ID, "smileImage").get_attribute("naturalHeight"))
+
+
 
 time.sleep(3)
