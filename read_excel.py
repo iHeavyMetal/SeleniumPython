f63f498183eb10ed9dbc5f9d8c3d4312d3e5e0b6
HeavyMetal <dziobal294@gmail.com>
2023-01-21 17:02:43 +0100

    Excel read

---
+++ read_excel.py
@@ -0,0 +1,7 @@
+import xlrd
+
+wb = xlrd.open_workbook("Dane.xls")
+sheet = wb.sheet_by_index(0)
+for i in range(1,sheet.nrows):
+    for j in range(sheet.ncols):
+        print(sheet.cell(i, j).value)
